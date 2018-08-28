ce75088d68b1caa8951f67d9cc6ff10301d0c0c2
pcwii <30759206+pcwii@users.noreply.github.com>
2018-08-28 19:24:35 -0400

    Add files via upload

---
+++ CinemaStart.py
@@ -0,0 +1,42 @@
+import sys
+from websocket import create_connection
+import json
+import time
+
+URL_TEMPLATE = "{scheme}://{host}:{port}{path}"
+
+
+socket_host = "192.168.0.41" #this should be the ip address of the mycroft device on the local network
+socket_port = 8181 #this port should match the client port
+
+
+def send_message(message, host=socket_host, port=socket_port, path="/core", scheme="ws"):
+    payload = json.dumps({
+        "type": "recognizer_loop:utterance",
+        "context": "",
+        "data": {
+            "utterances": [message]
+        }
+    })
+    url = URL_TEMPLATE.format(scheme=scheme, host=host, port=str(port), path=path)
+    ws = create_connection(url)
+    ws.send(payload)
+    ws.close()
+
+
+# mute Command
+send_message('PleaseBeSilent')
+time.sleep(1)
+
+send_message('turn the wall lights on')
+time.sleep(2)
+
+send_message('turn the room lights off')
+send_message('set the wall lights to orange')
+time.sleep(15)
+
+# un-mute Command
+send_message('YouCanSpeakNow')
+print('no longer silent')
+
+
